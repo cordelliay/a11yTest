02c6c3799c2875a348e24adf922fd1f90a6abb9c
Cordellia Yokum <89933022+cordellia-SO5@users.noreply.github.com>
2022-11-05 15:39:44 -0400

    add build docs & nojekyll

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,19 @@
 import adapter from '@sveltejs/adapter-static';
+const dev = "production" === "development";
 
 export default {
   kit: {
     adapter: adapter({
       // default options are shown. On some platforms
       // these options are set automatically — see below
-      pages: 'build',
-      assets: 'build',
+      pages: 'docs',
+      assets: 'docs',
       fallback: null,
       precompress: false,
       strict: true
-    })
+    }),
+	paths: {
+		base: dev ? "" : "/a11yTest",
+	}
   }
 };
