962a4234c474a4baa525cf6abca83fc96093b7fc
Cordellia Yokum <89933022+cordellia-SO5@users.noreply.github.com>
2022-11-05 15:26:30 -0400

    initial commit

---
+++ gh-pages.js
@@ -0,0 +1,17 @@
+import { publish } from 'gh-pages';
+
+publish(
+ 'build', // path to public directory
+ {
+  branch: 'gh-pages',
+  repo: 'https://github.com/cordelliay/a11yTest.git',
+  user: {
+   name: 'cordelliay', // update to use your name
+   email: 'cordelliay@hotmail.com' // Update to use your email
+  },
+  dotfiles: true
+  },
+  () => {
+   console.log('Deploy Complete!');
+  }
+);
